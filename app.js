ae6c0cc094e74b486b2bf434ec99f8bfbd00732e
Ghouse Moinuddin <ghousemoinuddin118@gmail.com>
2026-07-26 23:56:55 +0530

    Initial

--- app.js
+++ app.js
@@ -109,6 +109,16 @@ app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
+const validateListing = (req, res,next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else {
+        next();
+    }
+}
+
 //index route
 app.get("/listings", wrapAsync(async (req, res)=> {
     let allListings = await Listing.find({});
@@ -128,7 +138,7 @@ app.get("/listings/:id", WrapASync(async (req,res) => {
 }));
 
 //Create Route
-app.post("/listings", wrapAsync (async (req, res, next)=> {
+app.post("/listings", validateListing, wrapAsync (async (req, res, next)=> {
     // let {title,description, image, price, country, location} = req.body;
     // let listing = req.body.listing;
         const newListing = new Listing(req.body.listing);
@@ -145,7 +155,7 @@ app.get("/listings/:id/edit", WrapASync(async (req,res) => {
 }));
 
 //update route
-app.put("/listings/:id", wrapAsync(async (req,res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req,res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
