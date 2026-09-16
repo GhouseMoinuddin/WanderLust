eba9107e9ce97e01d2cd78bbb04b667fecc4d8e3
GhouseMoinuddin <ghousemoinuddin118@gmail.com>
2026-09-16 23:49:42 +0530

    allListings route

--- backend/app.js
+++ backend/app.js
@@ -7,47 +7,52 @@ import Listing from "./models/listing.js";
 dotenv.config();
 const app = express();
 
-app.set("port",(process.env.PORT || 8000));
+app.set("port", (process.env.PORT || 8000));
 app.use(cors());
-app.use(express.json({limit:"40kb"}));
-app.use(express.urlencoded({limit:"40kb", extended:true}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.get("/home", (req,res)=> {
-    return res.json({"hello":"world"});
+app.get("/home", (req, res) => {
+    return res.json({ "hello": "world" });
 });
 
-app.get("/", (req,res) => {
-      res.send("Hi, I am root");
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
 })
 
-app.get("/test", async (req,res) => {
-    let sampleListing = new Listing({
-        title:"My home villa",
-        description:"welcome",
-        price:"2000",
-        location:"hyderabad",
-        country:"India"
-    });
-
-    await sampleListing.save();
-    console.log("Done");
-    res.send("testing done!");
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("index.ejs", { allListings });
 })
+
+// app.get("/test", async (req,res) => {
+//     let sampleListing = new Listing({
+//         title:"My home villa",
+//         description:"welcome",
+//         price:"2000",
+//         location:"hyderabad",
+//         country:"India"
+//     });
+
+//     await sampleListing.save();
+//     console.log("Done");
+//     res.send("testing done!");
+// })
 
 const start = async () => {
     try {
         const MONGO_URL = process.env.MONGODB_URL;
         const connectionDb = await mongoose.connect(MONGO_URL);
-        
-        console.log(`mongo Connected DB Host: ${connectionDb.connection.host}`);    
-        app.listen(app.get("port"), ()=> {
+
+        console.log(`mongo Connected DB Host: ${connectionDb.connection.host}`);
+        app.listen(app.get("port"), () => {
             console.log("server is listening on port 8000");
         })
-    }catch(error) {
-        console.error("MongoDB connection failed!",  error.message);
+    } catch (error) {
+        console.error("MongoDB connection failed!", error.message);
     }
 
-    
+
 }
 
 start();
